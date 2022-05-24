8d78066c9433f6ea6311f27122e7aa7258d0f762
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2022-05-25 01:40:24 +0200

    zuri training task read from a text file with python

---
+++ main.py
@@ -0,0 +1,33 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!")
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here
+    with open(filename) as file:
+        content = file.read()
+        print(content)
+    return content
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # first we turn the text read from the text file and place it in an array
+    text_array = text.split(" ")
+    # now we must create a unique list of words from the original text list
+    unique_array = []
+    result = {}
+    # the result is a dictionary that will be populated later
+    for word in text_array:
+        if word not in unique_array:
+            unique_array.append(word)
+    # we append the word to the list if the list did not already have the word as an element
+    # we must now tally the number of repeated words
+    for unique_word in unique_array:
+        result[unique_word] = text_array.count(unique_word)
+
+    return result
+
+
+print(count_words())
